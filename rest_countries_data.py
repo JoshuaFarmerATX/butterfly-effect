d5286b58d06553d800bc00ae119f4cbac3d5e7a0
JoshuaFarmerATX <53586634+JoshuaFarmerATX@users.noreply.github.com>
2020-01-11 10:16:04 -0600

    Turned the Rest Countries py file into a function

--- rest_countries_data.py
+++ rest_countries_data.py
@@ -4,34 +4,32 @@ import json
 import pprint
 import matplotlib as plt
 
-r = requests.get("https://restcountries.eu/rest/v2/all")
-raw_data = r.json()
-# pprint.pprint(raw_data)
+def get_rest_countries():
+    r = requests.get("https://restcountries.eu/rest/v2/all")
+    raw_data = r.json()
 
-# df = pd.DataFrame([{
-#     "City": d["name"]
-# } for ])
-
-def extract_block(field_name, d):
-    try:
-        return d[field_name][0]["name"]
-    except:
-        pass
+    def extract_block(field_name, d):
+        try:
+            return d[field_name][0]["name"]
+        except:
+            pass
 
 
-df = pd.DataFrame([{
-    "Country": d["name"],
-    "Alpha3 Code": d['alpha3Code'],
-    "Capital": d['capital'],
-    "Region": d['region'],
-    "Sub-Region": d['subregion'],
-    "Population": d['population'],
-    "Lat and Lon": d['latlng'],
-    "Area": d['area'],
-    "Timezone": d['timezones'],
-    "borders": d['borders'],
-    "Currencies": extract_block('currencies', d),
-    "Regional Bloc": extract_block('regionalBlocs', d)
-    } for d in raw_data])
+    return pd.DataFrame([{
+        "Country": d["name"],
+        "Alpha3 Code": d['alpha3Code'],
+        "Capital": d['capital'],
+        "Region": d['region'],
+        "Sub-Region": d['subregion'],
+        "Population": d['population'],
+        "Lat and Lon": d['latlng'],
+        "Area": d['area'],
+        "Timezone": d['timezones'][0],
+        "borders": d['borders'],
+        "Currencies": extract_block('currencies', d),
+        "Regional Bloc": extract_block('regionalBlocs', d)
+        } for d in raw_data])
 
+df = get_rest_countries()
 print(df.head())
+print(len(df['Country']))
